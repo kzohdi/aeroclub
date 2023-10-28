16d9c6d008633d308025f4482e706e188c206447
Karim Zohdi <karim.zohdi@gmail.com>
2023-10-28 13:50:57 +0200

    Initial commit

---
+++ src/main/java/com/spoofy/aeroclub/AeroclubApplication.java
@@ -0,0 +1,13 @@
+package com.spoofy.aeroclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spoofy/aeroclub/AeroclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spoofy.aeroclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
