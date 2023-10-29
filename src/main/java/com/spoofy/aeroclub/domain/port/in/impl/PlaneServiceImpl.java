0ebd759a53a717e2a0770b1a13ef1ac667bc29a0
Karim Zohdi <karim.zohdi@gmail.com>
2023-10-29 18:14:55 +0100

    Improve logging (#4)

--- src/main/java/com/spoofy/aeroclub/domain/port/in/impl/PlaneServiceImpl.java
+++ src/main/java/com/spoofy/aeroclub/domain/port/in/impl/PlaneServiceImpl.java
@@ -3,19 +3,22 @@ package com.spoofy.aeroclub.domain.port.in.impl;
 import com.spoofy.aeroclub.domain.model.Plane;
 import com.spoofy.aeroclub.domain.port.in.PlaneService;
 import com.spoofy.aeroclub.domain.port.out.PlaneRepository;
-import com.spoofy.aeroclub.exception.NotFoundException;
-import com.spoofy.aeroclub.exception.ServerException;
+import com.spoofy.aeroclub.common.exception.NotFoundException;
+import com.spoofy.aeroclub.common.exception.ServerException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PlaneServiceImpl implements PlaneService {
 
     private final PlaneRepository planeRepository;
 
     @Override
     public Plane createPlane(Plane plane) {
+        log.info("Create plane {}", plane);
         Plane existingPlane = planeRepository.findPlane(plane.getRegistration());
         if (null != existingPlane) {
             throw new ServerException("The plane you are trying to create already exists, try another one.");
@@ -25,6 +28,7 @@ public class PlaneServiceImpl implements PlaneService {
 
     @Override
     public Plane findPlane(String registration) {
+        log.info("Find plane {}", registration);
         Plane existingPlane = planeRepository.findPlane(registration);
         if (null == existingPlane) {
             throw new NotFoundException(String.format("The plane with the '%s' registration was not found.", registration));
